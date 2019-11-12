3cae29ffcfc7985299e0ebf1db81c0854be0c9bf
Miguel Velez <miguelvelez@mijecu25.com>
2019-11-12 09:59:34 -0500

    added memory monitor

---
+++ src/main/java/com/mijecu25/meme/utils/monitor/memory/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonNullByDefault
+package com.mijecu25.meme.utils.monitor.memory;
+
+import com.mijecu25.meme.utils.annotations.ReturnTypesAreNonNullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
