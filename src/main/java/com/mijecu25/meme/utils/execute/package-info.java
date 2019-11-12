7c308f5a6c9330c7e870b3ce3282aa5e5cf32e7d
Miguel Velez <miguelvelez@mijecu25.com>
2019-11-12 09:57:23 -0500

    added executor

---
+++ src/main/java/com/mijecu25/meme/utils/execute/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonNullByDefault
+package com.mijecu25.meme.utils.execute;
+
+import com.mijecu25.meme.utils.annotations.ReturnTypesAreNonNullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
